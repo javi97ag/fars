2316d13af9c99eadef6a05d95f8a787065177f0f
Javier Arias <javierarias55@gmail.com>
2018-07-14 14:13:28 +0200

    Tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,3 @@ library(testthat)
 library(fars)
 
 test_check("fars")
-test_that("Filename",{
-  expect_equal(fars_Simple::make_filename(2013), "accident_2013.csv.bz2")
-})
--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -1,7 +1,5 @@
-library(testthat)
-library(fars)
+context("Generic test")
 
-test_check("fars")
-test_that("Filename",{
-  expect_equal(fars::make_filename(2013), "accident_2013.csv.bz2")
+test_that("file name is correct", {
+  expect_equal(make_filename(2013), "accident_2013.csv.bz2")
 })
